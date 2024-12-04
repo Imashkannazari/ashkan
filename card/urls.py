b8b9347f34192f4989ed584c1df69ee8f7095bb6
ashkan <imashkannazari@gmail.com>
2024-12-05 00:48:51 +0330

    create home

--- card/urls.py
+++ card/urls.py
@@ -8,5 +8,4 @@ app_name = 'card'
 urlpatterns = [
     path('projects/', views.card, name='card'),
     path('projects/<slug:slug>/', views.detail, name='card_detail'),
-    path('programmers/', views.programmers, name='programmers'),
 ]
--- card/views.py
+++ card/views.py
@@ -3,12 +3,8 @@ from . models import Projects, Programmers
 from django.shortcuts import get_object_or_404
 def card(request):
     project = Projects.objects.all()
-    return render(request, 'projects list.html', {"project":project})
+    return render(request, 'card/projects list.html', {"project":project})
 
 def detail(request, slug):
     projects=get_object_or_404(Projects, slug=slug)
-    return render(request, 'detail.html',   {'projects':projects})
-
-def programmers(request):
-    programmer = Programmers.objects.all()
-    return render(request, 'programmers list.html', {"programmer":programmer})
+    return render(request, 'card/detail.html', {'projects':projects})
